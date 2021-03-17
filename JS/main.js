2e3e12c3a657c191feab6d90c93198de7726799e
Sabrina <sanutaayvi@gmail.com>
2021-03-17 18:56:22 +0600

    updated

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,63 @@
 // API
 
 const  API_KEY = 'c6f56dd60e130c06ab6e02838322c1db';
-const url = 'https://api.themoviedb.org/3/search/movie?api_key=c6f56dd60e130c06ab6e02838322c1db&query=furious';
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=c6f56dd60e130c06ab6e02838322c1db';
+const  IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
 
 
 // selecting   values  from DOM
 
 const  buttoneElement = document.querySelector('#search');
 const  inputElement = document.querySelector('#inputValue');
+const  movieSearchable = document.querySelector('#movies-searchable');
+
+
+
+function movieSection(movies){
+  return  movies.map((movie) =>{
+        return `
+        <img src = ${IMAGE_URL + movie.poster_path} data-movie-id = ${movie.id}>
+        `;
+    } )
+}
+
+function  createMovieContainer(movies){
+    const  movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+    const  movieTemplete =`
+    <section class="section">
+                 
+
+    ${movieSection(movies)}
+
+    </section>
+<div  class='content'>
+<p id="content-close">X</p>
+
+             `;
+            movieElement.innerHTML = movieTemplete;
+            return movieElement;
+
+}
+
+
 
 
 buttoneElement.onclick = function(event){
     event.preventDefault();
 
 const  value  =  inputElement.value;
-fetch(url)
+
+const  newUrl = url + '&query=' + value;
+
+fetch(newUrl)
   .then((res)=>res.json())
   .then((data)=> {
+      const  movies = data.results;
+
+     const movieBlock =  createMovieContainer(movies);
+
+     movieSearchable.appendChild(movieBlock);
       console.log('Data: ',  data);
 
   })
