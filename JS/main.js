00654cc42584f68b8d9f7112311321e5f2d2a807
Sabrina <sanutaayvi@gmail.com>
2021-03-17 12:55:37 +0600

    featching data

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,9 @@
+// API
+
+const  API_KEY = 'c6f56dd60e130c06ab6e02838322c1db';
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=c6f56dd60e130c06ab6e02838322c1db&query=furious';
+
+
 // selecting   values  from DOM
 
 const  buttoneElement = document.querySelector('#search');
@@ -8,6 +14,17 @@ buttoneElement.onclick = function(event){
     event.preventDefault();
 
 const  value  =  inputElement.value;
+fetch(url)
+  .then((res)=>res.json())
+  .then((data)=> {
+      console.log('Data: ',  data);
+
+  })
+  .catch((error)=>{
+      console.log('Error: ', error);
+
+  });
+
 console.log('Value: ', value);
 
 }
